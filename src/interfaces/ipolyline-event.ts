f5ab7d1e69cdffd46273420006a9e6bccc54bd3e
Joe Mellon <mellonj@etech.net>
2020-09-14 09:43:37 +0100

    .l..

--- src/interfaces/ipolyline-event.ts
+++
@@ -1,9 +0,0 @@
-﻿import { MapPolylineDirective } from '../components/map-polyline';
-import { Polyline } from '../models/polyline';
-import { ILatLong } from './ilatlong';
-import { IPoint } from './ipoint';
-
-export interface IPolylineEvent {
-    Polyline: MapPolylineDirective|Polyline;
-    Click: MouseEvent;
-}
